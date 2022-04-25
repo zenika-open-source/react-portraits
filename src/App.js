1f0492f436e1d828f7263e4bc062da5295eb2177
Ismail El Khantache <ismail.elkhantache@zenika.com>
2022-04-25 10:37:58 +0200

    Update global style

--- src/App.js
+++ src/App.js
@@ -1,61 +1,75 @@
 import Portraits from './lib/components/Portraits';
+import React, { useState } from 'react';
 import './App.css';
+
+const portraits = [
+  {
+    leftPortrait: {
+      img: "1.png",
+      name: "Charlie Boyer",
+      skills: [
+        {
+          img: "Agilité.svg",
+          name: "Agilité"
+        },
+        {
+          img: "Architecture.svg",
+          name: "Architecture"
+        }
+      ]
+    },
+    rightPortrait: {
+      img: "2.png",
+      name: "Charlotte Colliou",
+      skills: [
+        {
+          img: "Data.svg",
+          name: "Data"
+        }
+      ]
+    }
+  },
+  {
+    leftPortrait: {
+      img: "3.png",
+      name: "Jeanne Marcade",
+      skills: [
+        {
+          img: "Design.svg",
+          name: "Design"
+        }
+      ]
+    },
+    rightPortrait: {
+      img: "4.png",
+      name: "William JEZEQUEL",
+      skills: [
+        {
+          img: "DevOps.svg",
+          name: "DevOps"
+        }
+      ]
+    }
+  }
+
+]
 
 function App() {
 
-  const portraits = [
-    {
-      leftPortrait: {
-        img: "1.png",
-        name: "Charlie Boyer",
-        skills: [
-          {
-            img: "skill1.png",
-            name: "Web"
-          },
-          {
-            img: "skill2.png",
-            name: "cloud"
-          }
-        ]
-      },
-      rightPortrait: {
-        img: "2.png",
-        name: "Charlotte Colliou",
-        skills: [
-          {
-            img: "skill4.png",
-            name: "ui"
-          }
-        ]
-      }
-    },
-    {
-      leftPortrait: {
-        img: "3.png",
-        name: "Jeanne Marcade",
-        skills: [
-          {
-            img: "skill1.png",
-            name: "web"
-          }
-        ]
-      },
-      rightPortrait: {
-        img: "4.png",
-        name: "William JEZEQUEL",
-        skills: [
-          {
-            img: "skill3.png",
-            name: "ia"
-          }
-        ]
-      }
-    }
 
-  ]
+  const [index, setIndex] = useState(0)
+
+
+  function nextTeamMembers(e) {
+    e.preventDefault();
+    console.log("Next team members...")
+
+    setIndex((index + 1)%portraits.length)
+
+  }
 
   return (
+    
     <div style={{ display:'flex', 
               flexDirection:'column', 
               alignItems:'start', 
@@ -63,11 +77,19 @@ function App() {
               gap:'1.5rem', 
               padding: '5rem' }}>
 
-      <p>Component settings:</p>
+      <h1>React Portraits</h1>
+      <div className='jss67'>
+        <p>Component settings:</p>
 
-      <p>Wow, look at this component library.</p>
-      <h5>A magnificent team:</h5>
-      <Portraits portraits={portraits} index={0} />
+        <button onClick={nextTeamMembers}>next</button>
+
+        <p>Wow, look at this component library.</p>
+        <h5>A magnificent team:</h5>
+      </div>
+
+
+
+      <Portraits portraits={portraits} index={index} />
     </div>
   );
 }
--- src/lib/components/Portraits.js
+++ src/lib/components/Portraits.js
@@ -1,46 +1,38 @@
 import './styles/Portraits.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSpring, animated, config } from 'react-spring'
 
 function Portraits(props) {
 
-  const [leftFlip, setLeftFlip] = useState(false)
-  const leftAnim = useSpring({
-      to: { opacity: 1 },
-      from: { opacity: 0 },
-      reset: false,
-      delay: 200,
-      config: config.molasses,
-      onRest: () => setLeftFlip(!leftFlip),
-  })
+  const [leftAnim, api] = useSpring(() => ({
+    from: { opacity: 0 },
+    to: { opacity: 1 },
+    reset: false,
+    delay: 200,
+    config: config.molasses
+}))
 
-  const [rightFlip, setRightFlip] = useState(false)
   const rightAnim = useSpring({
       to: { opacity: 1 },
       from: { opacity: 0 },
       reset: false,
       delay: 3000,
-      config: config.molasses,
-      onRest: () => setRightFlip(!rightFlip),
+      config: config.molasses
   })
 
-  const [index, setIndex] = useState(props.index)
-
-  // useEffect(() => {
-  //     const intervalID = setInterval(() => {
-  //         console.log("Next portrait : ", index, " => ", index + 1 >= props.portraits.length ? 0 : index + 1)
-  //         setIndex(index + 1 >= props.portraits.length ? 0 : index + 1)
-  //     }, 5000)
-  //   }, []);
-
+  useEffect(() => {
+    console.log("PROPS INDEX CHANGES")
+    console.log(leftAnim, api)
+  }, [props.index])
+  
   return (
       <div className='portraits-wrapper'>
           <div className='left-text'>
               <animated.div style={leftAnim}>
-                  <span className='skill-text'>{props.portraits[index].leftPortrait.name}</span>
+                  <span className='skill-text'>{props.portraits[props.index].leftPortrait.name}</span>
 
                   <div>
-                      {props.portraits[index].leftPortrait.skills.map((v,i) => {
+                      {props.portraits[props.index].leftPortrait.skills.map((v,i) => {
                           return <img alt="Skill" key={"left-"+i} className='skill-img' src={v.img} />
                       })}
                   </div>
@@ -48,17 +40,17 @@ function Portraits(props) {
           </div>
           <div className='portraits-frame'>
               <animated.div style={leftAnim}>
-                  <img alt="Team member" className='portrait-left' src={props.portraits[index].leftPortrait.img} />
+                  <img alt="Team member" className='portrait-left' src={props.portraits[props.index].leftPortrait.img} />
               </animated.div>
               <animated.div style={rightAnim}>
-                  <img alt="Team member" className='portrait-right' src={props.portraits[index].rightPortrait.img} />
+                  <img alt="Team member" className='portrait-right' src={props.portraits[props.index].rightPortrait.img} />
               </animated.div>
           </div>
           <div className='right-text'>
               <animated.div style={rightAnim}>
                   <span className='skill-text'>{props.portraits[0].rightPortrait.name}</span>
                   <div>
-                      {props.portraits[index].rightPortrait.skills.map((v,i) => {
+                      {props.portraits[props.index].rightPortrait.skills.map((v,i) => {
                               return <img alt="Skill"key={"right-"+i} className='skill-img' src={v.img} />
                           })}
                   </div>
